01581ed4d1f25f440a0d2f4d03e33f87040b0302
Willy Gonçalves Campos <willygoncalvescampos@gmail.com>
2025-06-17 19:36:00 -0300

    create project

---
+++ src/main/java/edu/ifmg/hotelBAO/HotelBaoApplication.java
@@ -0,0 +1,13 @@
+package edu.ifmg.hotelBAO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ifmg/hotelBAO/HotelBaoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ifmg.hotelBAO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

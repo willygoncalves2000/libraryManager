c12b63fc4c1ca6bd1ed4d0ff689e690ffee356ee
Willy Gonçalves Campos <willygoncalvescampos@gmail.com>
2025-07-10 15:00:12 -0300

    teste com jUnit

---
+++ src/test/java/edu/ifmg/hotelBAO/repository/RoomRepositoryTest.java
@@ -0,0 +1,27 @@
+package edu.ifmg.hotelBAO.repository;
+
+import edu.ifmg.hotelBAO.entities.Room;
+import edu.ifmg.hotelBAO.util.Factory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.util.Assert;
+
+import java.util.Optional;
+
+@DataJpaTest
+public class RoomRepositoryTest {
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @Test
+    @DisplayName("Verificando se o objeto não existe no BD depois de ser deletado.")
+    public void deleteShoulDeleteObjectWhenIdExists() {
+        roomRepository.deleteById(1L);
+        Optional<Room> optional = roomRepository.findById(1L);
+        Assertions.assertFalse(optional.isPresent());
+    }
+}
---
+++ src/test/java/edu/ifmg/hotelBAO/util/Factory.java
@@ -0,0 +1,21 @@
+package edu.ifmg.hotelBAO.util;
+
+import edu.ifmg.hotelBAO.dtos.RoomDTO;
+import edu.ifmg.hotelBAO.entities.Room;
+
+import java.math.BigDecimal;
+
+public class Factory {
+    public static Room createRoom() {
+        Room room = new Room();
+        room.setName("Room 1");
+        room.setDescription("Room 1 Description");
+        room.setImageUrl("Room 1 Image");
+        room.setPrice(BigDecimal.valueOf(100));
+        return room;
+    }
+    public static RoomDTO createRoomDTO() {
+        Room r = createRoom();
+        return new RoomDTO(r);
+    }
+}
